a496c46b1b8b07099ab8ba8ba2bff9a191f91641
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-18 19:44:13 -0700

    reorganize program structure

--- server/test.go
+++
@@ -1,9 +0,0 @@
-package other
-
-type Keys struct {
-  Key, Secret string
-}
-
-var (
-  Giggle = Keys{"687671941121-lnojr5i0f2eknrdpivcdah6pfhiruro7.apps.googleusercontent.com", "bHG1-oSVkf0MOs9QSwTJvS_P"}
-)
