07a80eed275115cf9dab5ab1a96e3fffe10f796f
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-19 22:45:15 -0700

    initial react-native setup

--- server/main.go
+++ server/main.go
@@ -1,72 +1,13 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "golang.org/x/oauth2"
-  "golang.org/x/oauth2/google"
-  "io/ioutil"
+	"log"
+	"net/http"
 )
-// "github.com/gorilla/mux" add to import
-//  "os"
-
-const htmlIndex = `<html><body>
-<a href="/GoogleLogin">Log the fuck in with Google</a>
-</body></html>
-`
-
-func handleMain(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, htmlIndex)
-}
-
-var (
-    googleOauthConfig = &oauth2.Config{
-        RedirectURL:    "http://localhost:3000/GoogleCallback",
-        // ClientID:     os.Getenv("googlekey"),
-        ClientID:     Google.Key,
-        // ClientSecret: os.Getenv("googlesecret"),
-        ClientSecret: Google.Secret,
-        Scopes:       []string{"https://www.googleapis.com/auth/userinfo.profile",
-            "https://www.googleapis.com/auth/userinfo.email"},
-        Endpoint:     google.Endpoint,
-    }
-// Some random string, random for each request
-    oauthStateString = "random"
-)
-
-
-
-func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
-    url := googleOauthConfig.AuthCodeURL(oauthStateString)
-    http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-}
-
-func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
-    state := r.FormValue("state")
-    if state != oauthStateString {
-        fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
-        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-        return
-    }
-
-    code := r.FormValue("code")
-    token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
-    if err != nil {
-        fmt.Println("Code exchange failed with '%s'\n", err)
-        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-        return
-    }
-
-    response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
-
-    defer response.Body.Close()
-    contents, err := ioutil.ReadAll(response.Body)
-    fmt.Fprintf(w, "Content: %s\n", contents)
-}
 
 func main() {
-    http.HandleFunc("/", handleMain)
-    http.HandleFunc("/GoogleLogin", handleGoogleLogin)
-    http.HandleFunc("/GoogleCallback", handleGoogleCallback)
-    fmt.Println(http.ListenAndServe(":3000", nil))
+
+	router := NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
