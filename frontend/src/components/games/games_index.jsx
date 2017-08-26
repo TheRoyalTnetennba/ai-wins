62799548ff5ea9ee7a047706c991f9c23c79630b
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-26 02:08:31 -0400

    rework initial react setup and switch to yarn

---
+++ frontend/src/components/games/games_index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class GamesIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    
+  }
+}
+
+export default GamesIndex;
