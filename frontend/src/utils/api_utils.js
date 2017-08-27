4492357bfb20f0bb6884eff97751d677f6a251f2
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-27 10:39:05 -0700

    add eslint

--- frontend/src/utils/api_utils.js
+++ frontend/src/utils/api_utils.js
@@ -1,5 +1,18 @@
-import fetch from 'fetch-everywhere';
+import sha512 from 'sha512';
 
 const baseURL = 'https://kitchenfox.herokuapp.com/api/';
 
-// https://github.com/github/fetch#post-json for how-to
+export const getAIMove = (gameName, gameState, misc) => {
+  const hashName = sha512(gameName).toString('hex')
+  return fetch(`/getMove/${hashName}`, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      gameID: hashName,
+      gameState,
+      misc,
+    })
+  })
+}
