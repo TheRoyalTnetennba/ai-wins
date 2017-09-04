b9b5c0d194fad0a8d659f84942411547ffe82c74
TheRoyalTnetennba <gpaye8@gmail.com>
2017-09-03 22:01:11 -0700

    fix GetMatrixFromInterface function

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -1,7 +1,5 @@
 package utils
 
-import "fmt"
-
 // GetMatrixFromInterface processes simplejson-created interfaces of a stringified matrix
 func GetMatrixFromInterface(inter interface{}) [][]string {
 	var matrix [][]string
@@ -14,15 +12,5 @@ func GetMatrixFromInterface(inter interface{}) [][]string {
 		}
 		matrix = append(matrix, row)
 	}
-	// arr := reflect.Value(reflect.ValueOf(inter))
-	// for i := 0; i < arr.Len(); i++ {
-	// 	rowInt := reflect.Value(reflect.ValueOf(arr))
-	// 	fmt.Println(rowInt)
-	// 	// var row []string
-	// 	// for j := 0; j < rowInt.Len(); j++ {
-	// 	// 	fmt.Println(rowInt.Index(j), rowInt.Kind())
-	// 	// }
-	// }
-	fmt.Println(matrix)
 	return matrix
 }
