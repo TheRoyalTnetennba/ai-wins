6b761b17e2d768415bf6dc8d71cdc65498611afb
TheRoyalTnetennba <gpaye8@gmail.com>
2017-09-03 21:14:17 -0700

    .

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -1,19 +1,25 @@
 package utils
 
 import (
+	"fmt"
 	"reflect"
 )
 
 // GetMatrixFromInterface processes simplejson-created interfaces of a stringified matrix
 func GetMatrixFromInterface(inter interface{}) [][]string {
 	var matrix [][]string
-	arr := reflect.ValueOf(inter)
-	for i := 0; i < arr.Len(); i++ {
+	for i := 0; i < 3; i++ {
 		var row []string
-		for j := 0; j < arr.Len(); j++ {
-			row = append(row, arr.Index(i).Index(j).String())
+		for j := 0; j < 3; j++ {
+			row = append(row, "")
 		}
 		matrix = append(matrix, row)
+	}
+	arr := reflect.ValueOf(inter)
+	for i := 0; i < arr.Len(); i++ {
+		for j := 0; j < arr.Len(); j++ {
+			fmt.Println(arr.Index(i).Kind())
+		}
 	}
 	return matrix
 }
