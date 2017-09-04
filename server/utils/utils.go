ee8b2553059e48728532259d998ba4097b857b08
TheRoyalTnetennba <gpaye8@gmail.com>
2017-09-03 19:33:57 -0700

    fix capitalization for function export

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func getMatrixFromInterface(arr []interface{}) [][]string {
+func GetMatrixFromInterface(arr []interface{}) [][]string {
 	var matrix [][]string
 	for _, i := range arr {
 		matrix = append(matrix, i.([]string))
