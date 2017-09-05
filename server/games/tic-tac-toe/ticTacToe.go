d7a0156dbae2de96c7b52ac9779a20d6c1d0a2bc
TheRoyalTnetennba <gpaye8@gmail.com>
2017-09-05 09:55:41 -0700

    begin polynode for ttt ai

--- server/games/tic-tac-toe/ticTacToe.go
+++ server/games/tic-tac-toe/ticTacToe.go
@@ -1,63 +1,82 @@
 package tttAI
 
-// func checkTriplet(triplet []string) string {
-// 	if triplet[0] == triplet[1] && triplet[1] == triplet[2] {
-// 		return triplet[0]
-// 	}
-// 	for i := 0; i < len(triplet); i++ {
-// 		if triplet[i] == "" {
-// 			return "Incomplete"
-// 		}
-// 	}
-// 	return "Contested"
-// }
-// func win(tripletResponse string) bool {
-// 	return tripletResponse == "x" || tripletResponse == "o"
-// }
+type gameNode struct {
+	Board      [][]string
+	NextBoards []gameNode
+	Result     int
+}
 
-// func isGameOver(board [][]string) bool {
-// 	numIncompletes := 0
-// 	for row := 0; row < len(board); row++ {
-// 		if win(checkTriplet(board[row])) {
-// 			return true
-// 		}
-// 		var colRow []string
-// 		for col := 0; col < len(board[row]); col++ {
-// 			colRow = append(colRow, board[col][row])
-// 		}
-// 		carlRove := checkTriplet(colRow)
-// 		if win(carlRove) {
-// 			return true
-// 		} else if carlRove == "Incomplete" {
-// 			numIncompletes++
-// 		}
-// 		if win(checkTriplet(colRow)) {
-// 			return true
-// 		}
-// 	}
-// 	var upRight []string
-// 	var upLeft []string
-// 	for i := 0; i < len(board); i++ {
-// 		upRight = append(upRight, board[i][len(board[i])-i])
-// 		upLeft = append(upLeft, board[i][i])
-// 	}
-// 	if win(checkTriplet(upRight)) || win(checkTriplet(upLeft)) {
-// 		return true
-// 	}
-// 	return numIncompletes == 0
-// }
+func xit(game gameNode) {
 
-func GetAIMove(board [][]string, marker string) []int {
-	if len(board[1][1]) == 0 {
-		return []int{1, 1}
-	} else {
-		for i := 0; i < len(board); i++ {
-			for j := 0; j < len(board[i]); j++ {
-				if len(board[i][j]) == 0 {
-					return []int{i, j}
-				}
+}
+
+func genNodes(game gameNode, games []gameNode, marker string) []gameNode {
+
+	return games
+}
+
+func checkTriplet(triplet []string) string {
+	if triplet[0] == triplet[1] && triplet[1] == triplet[2] {
+		return triplet[0]
+	}
+	for i := 0; i < len(triplet); i++ {
+		if triplet[i] == "" {
+			return "Incomplete"
+		}
+	}
+	return "Contested"
+}
+func win(tripletResponse string) bool {
+	return tripletResponse == "x" || tripletResponse == "o"
+}
+
+func isGameOver(game gameNode) bool {
+	board := game.Board
+	numIncompletes := 0
+	for row := 0; row < len(board); row++ {
+		if win(checkTriplet(board[row])) {
+			return true
+		}
+		var colRow []string
+		for col := 0; col < len(board[row]); col++ {
+			colRow = append(colRow, board[col][row])
+		}
+		carlRove := checkTriplet(colRow)
+		if win(carlRove) {
+			return true
+		} else if carlRove == "Incomplete" {
+			numIncompletes++
+		}
+		if win(checkTriplet(colRow)) {
+			return true
+		}
+	}
+	var upRight []string
+	var upLeft []string
+	for i := 0; i < len(board); i++ {
+		upRight = append(upRight, board[i][len(board[i])-i])
+		upLeft = append(upLeft, board[i][i])
+	}
+	if win(checkTriplet(upRight)) || win(checkTriplet(upLeft)) {
+		return true
+	}
+	return numIncompletes == 0
+}
+
+func DumMove(board [][]string) []int {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			if len(board[i][j]) == 0 {
+				return []int{i, j}
 			}
 		}
 	}
 	return []int{0, 0}
+}
+
+func GetAIMove(board [][]string, marker string) []int {
+	if len(board[1][1]) == 0 {
+		return []int{1, 1}
+	}
+	return DumMove(board)
 }
