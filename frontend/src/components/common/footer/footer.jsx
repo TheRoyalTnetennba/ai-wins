431ec8987b1dd49584478c7c38c309f9a0798cb8
graham <gpaye8@gmail.com>
2017-09-27 00:18:04 -0700

    add social icons

--- frontend/src/components/common/footer/footer.jsx
+++ frontend/src/components/common/footer/footer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { SocialIcon } from 'react-social-icons';
 import './footer.css';
 
 class Footer extends Component {
@@ -7,13 +8,21 @@ class Footer extends Component {
   }
 
   mine() {
-
+    alert('hel')
   }
 
   render() {
     return (
       <footer>
-        <h1>footer</h1>
+        <section className="subsection">
+          <h1>Follow Me</h1>
+          <div className="social-buttons">
+            <SocialIcon url="https://www.grahampaye.com/" color="#D6655A" />
+            <SocialIcon url="https://www.linkedin.com/in/graham-paye/" color="#D6655A" />
+            <SocialIcon url="https://github.com/TheRoyalTnetennba" color="#D6655A" />
+            <SocialIcon network="email" onClick={() => this.mine()} color="#D6655A" className="cp" />
+          </div>
+        </section>
       </footer>
     );
   }
