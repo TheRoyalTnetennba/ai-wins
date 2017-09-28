d4563c7fffb80dbf931187b4b9317653e682ae97
graham <gpaye8@gmail.com>
2017-09-27 17:42:07 -0700

    minor refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
-import './App.css';
 import './components/common/atomics/colors.css';
 import './components/common/atomics/layout.css';
 import GamesIndex from './components/games/games_index';
