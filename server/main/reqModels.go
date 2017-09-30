76b9dfe78c687812c04eaa0cc01da508d538545b
graham <gpaye8@gmail.com>
2017-09-30 08:32:10 -0700

    add models

---
+++ server/main/reqModels.go
@@ -0,0 +1,25 @@
+type GoogleUser struct {
+    Email string
+    Family_name string
+    Gender string
+    Given_name string
+    Id string
+    Link string
+    Locale string
+    Name string
+    Picture string
+    Verified_email bool
+}
+
+type TTTMove struct {
+    Board [][]string
+    Marker string
+    Move [] int
+}
+
+type HangmanMove struct {
+    Board [][]string
+
+
+}
+
