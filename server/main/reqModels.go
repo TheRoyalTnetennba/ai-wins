10fef973615818aaa3d6fec42b513ac2ce40b01b
graham <gpaye8@gmail.com>
2017-09-29 19:49:11 -0700

    remove broken function

--- server/main/reqModels.go
+++ server/main/reqModels.go
@@ -20,13 +20,6 @@ type TTTMove struct {
 type HangmanMove struct {
     Board [][]string
 
+
 }
 
-func GetAIMove(body map[string]interface{}, c chan []byte) {
-    board := utils.GetMatrixFromInterface(body["gameState"])
-    pos := GetBestMove(board, body["marker"].(string))
-    res := simplejson.New()
-    res.Set("move", pos)
-    payload, _ := res.Encode()
-    c <- payload
-}
