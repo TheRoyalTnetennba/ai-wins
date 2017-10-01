3490f6d00f441b135b6003b0c4abc00d83f32755
graham <gpaye8@gmail.com>
2017-09-30 23:41:22 -0700

    bootstrap user

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,34 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/store';
+import { fetchCurrentUser } from './utils/api_utils';
+
+const cookie = (document.cookie.match(/^(?:.*;)?\s*ai-wins\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1]
+let store;
+if (cookie) {
+  fetchCurrentUser()
+    .then(response => response.json())
+    .then(user => {
+      let preload = {
+        session: {
+          currentUser: user,
+        },
+      };
+      store = configureStore(preload);
+      ReactDOM.render(<App store={ store } />, document.getElementById('root'));
+      registerServiceWorker();
+    })
+    .catch(() => {
+      store = configureStore();
+      ReactDOM.render(<App store={ store } />, document.getElementById('root'));
+      registerServiceWorker();
+    })
+  } else {
+    store = configureStore();
+    ReactDOM.render(<App store={ store } />, document.getElementById('root'));
+    registerServiceWorker();
+  }
 
 
-const store = configureStore();
-window.store = store;
-ReactDOM.render(<App store={ store } />, document.getElementById('root'));
-registerServiceWorker();
+
+
