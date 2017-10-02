ed70f02fc82dd96ceceabf9a103dd86355ce36c8
graham <gpaye8@gmail.com>
2017-10-02 08:03:35 -0700

    begin ttt actions

--- frontend/src/actions/ttt_actions.js
+++ frontend/src/actions/ttt_actions.js
@@ -1,7 +1,30 @@
-type TTTState struct {
-    User *datastore.Key
-    Marker string
-    Started time.Time
-    Board [][]string
-    Key *datastore.Key `datastore:"__key__"`
-}
+import { tttExchange } from '../utils/api_utils';
+
+export const RECEIVE_TTT = 'RECEIVE_TTT';
+
+export const receiveTTT = ttt => ({
+  type: RECEIVE_TTT,
+  ttt,
+});
+
+export const tttExchange = (request) => dispatch => (
+  fetchGames()
+    .then((response) => response.json())
+    .then(games => {
+      dispatch(receiveGames(games));
+      dispatch(clearErrors());
+    })
+    .catch(error => dispatch(receiveErrors(error)))
+);
+
+
+
+
+// type TTTState struct {
+//     User *datastore.Key
+//     Marker string
+//     Started time.Time
+//     Board [][]string
+//     Key *datastore.Key `datastore:"__key__"`
+// }
+
