50a27616a1b8ecc3a9ad1bc579e8bac6c367d1c2
graham <gpaye8@gmail.com>
2017-10-03 22:27:48 -0700

    a;

--- frontend/src/fire.js
+++ frontend/src/fire.js
@@ -1,9 +1,10 @@
+import firebase from 'firebase';
 var config = {
   apiKey: "AIzaSyBgJkSjHFrzypVo64z2QsoWo8kVyNjxpFc",
   authDomain: "ai-wins.firebaseapp.com",
   databaseURL: "https://ai-wins.firebaseio.com",
   storageBucket: "ai-wins.appspot.com",
-  messagingSenderId: "445753721865"
+  messagingSenderId: "445753721865",
 };
 var fire = firebase.initializeApp(config);
 export default fire;
