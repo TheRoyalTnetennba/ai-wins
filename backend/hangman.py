561baf495e433b06f7b6403370d7aca35ea7496d
graham <gpaye8@gmail.com>
2017-11-01 00:02:45 -0700

    add sowpods

--- backend/hangman.py
+++ backend/hangman.py
@@ -1,10 +1,25 @@
 class Hangman(object):
 	"""Hangman Game State. AI_role can either be 'g' for guesser or 's' for setter"""
-	def __init__(self, board, ai_role, misses, guess = None):
+	def __init__(self, sowpods, word, board, ai_role, misses, guess = None):
+		self.word = word
 		self.board = board
 		self.ai_role = ai_role
 		self.misses = misses
+		# find a better way to share sowpods
+		self.sowpods = sowpods
 		self.guess = None
 
 	def ai_move(self):
-		# if self.ai_role = 'g'
+		if self.ai_role = 's' and len(self.word) == 0: self.word = sowpods.get_random()
+		if self.ai_role = 'g':
+			matches = self.sowpods.find_matches(self.board, self.misses)
+			letters = {}
+			for word in matches:
+				for a in word.letters:
+					if a in letters: letters[a] += word.letters[a]
+					else: letters[a] = word.letters[a]
+			self.guess = max(letters, key=lambda k: letters[k])
+		if self.guess in self.word:
+			for i in range(len(self.word)):
+				if self.word[i] == self.guess: self.board[i] = self.guess
+		else: self.misses.append(self.guess)
--- backend/sowpods.py
+++ backend/sowpods.py
@@ -1,24 +1,31 @@
+import random
+
 """use pickle to avoid runnning everytime"""
-
-
 class Word(object):
     """Object for Word. Used by the Sowpod class"""
     def __init__(self, entry):
         self.entry = entry
         self.sorted = ''.join(sorted(entry))
         self.anagrams = []
+        self.letters = {}
+        self.count_letters()
 
-    def get_contents(self):
+    def count_letters(self):
+        for i in self.entry:
+            if i in self.letters: self.letters[i] += 1
+            else: self.letters[i] = 1
 
 
     def size(self):
         return len(self.entry)
 
 class Sowpod(object):
-    """Holds a collection of Scrabble dict words along with some useful methods for sorting and retreiving them"""
+    """Holds a collection of Scrabble dictionary words along with some useful methods for sorting and retreiving them"""
     def __init__(self):
         self.words = []
         self.anagrams = {}
+        self.letter_frequency = {}
+        self.read_txt()
 
     def read_txt(self):
         with open('resources/sowpods.txt') as sp:
@@ -27,5 +34,31 @@ class Sowpod(object):
                 word = Word(line)
                 if word.sorted in self.anagrams: self.anagrams[word.sorted].append(word)
                 else: self.anagrams[word.sorted] = [word]
-                
+                for letter in word:
+                    if letter in self.letter_frequency: self.letter_frequency[letter] = 1
+                    else: self.letter_frequency[letter] += 1
 
+    """optimize for speedy lookup perhaps with words_containing and words_excluding hashmaps or poly-tree"""
+    def find_matches(self, board, misses):
+        letters = {}
+        for letter in board:
+            if letter in letters: letters[letter] += 1
+            else: letters[letter] = 1
+        matches = []
+        for word in self.words:
+            if (word.size == len(board) and all(i not in misses for i in word.letters) and
+                all(board[i] == word.entry[i] or board[i] == '' for i in range(len(board)))):
+                matches.append(word)
+        return matches
+
+    def get_random(self, size = False, containing = False, excluding = False):
+        """containing and excluding should be lists of letters"""
+        # switch optional args to lamdas
+        sub_list = []
+        for word in self.words:
+            include = True
+            if size: include = include and word.size = size
+            if containing: include = include and all(i in word.letters for i in containing)
+            if excluding: include = include and none(i in word.letters for i in excluding)
+            if include: sub_list.append(word)
+        return random.choice(sub_list)
